8230228d5bd4d06c3e0a384ed32fe534e33540a1
hugochan <hugochan2013@gmail.com>
2015-05-17 18:46:23 +0800

    social/renren

---
+++ social/renren/renren_app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from renren import RenRen
+
+email = "1256679551@qq.com"
+passwd = '1993042887564299'
+renren = RenRen(email, passwd)
+# import pdb;pdb.set_trace()
+# friends_list = renren.get_friends()
+# print renren.search_profiles('陈宇', 20)
